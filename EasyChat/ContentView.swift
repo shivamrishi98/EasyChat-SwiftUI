5e4246528e3a85626feb67795d1ab5aeceecfffd
shivamrishi98 <shivamrishisr@gmail.com>
2020-07-29 15:37:07 +0530

    Initial Commit

---
+++ EasyChat/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  EasyChat
+//
+//  Created by Shivam Rishi on 29/07/20.
+//  Copyright © 2020 shivam. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
